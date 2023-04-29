85360dfe30f97449f6c21a673c8774ac40850201
I-Want-ToBelieve <floatshuyin@gmail.com>
2023-04-30 07:42:10 +0800

    feat(leagueoflegends): add package

--- pkgs/leagueoflegends/default.nix
+++ pkgs/leagueoflegends/default.nix
@@ -1,8 +1,9 @@
 { stdenv, fetchFromGitHub }:
 
 # @see https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=leagueoflegends-git
+# @see https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/misc/tdrop/default.nix
 stdenv.mkDerivation rec {
-  name = "leagueoflegends-${version}";
+  name = "leagueoflegends";
   version = "13.7.2";
   src = fetchFromGitHub ({
     owner = "kyechou";
@@ -11,5 +12,16 @@ stdenv.mkDerivation rec {
     fetchSubmodules = false;
     sha256 = "sha256-kzdPIceii39sfuesGGZCunXmBD01PL2b13zMbdwwxa4=";
   });
-  makeFlags = [ "DESTDIR=$out" ];
+
+  dontBuild = true;
+
+  installFlags = [ "PREFIX=$(out)" "DESTDIR=''"];
+
+  meta = with lib; {
+    description = "League of Legends install and launch wrapper for Linux";
+    homepage = "https://github.com/kyechou/leagueoflegends";
+    license = licenses.gpl3Only;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ ];
+  };
 }
